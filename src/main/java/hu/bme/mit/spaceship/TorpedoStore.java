0420c3a9412e0ff30735f71a360088d7ae02130b
LiamWalahfrid <fentosbence@gmail.com>
2019-05-03 13:36:00 +0200

    Bug fixed #6

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random rand = Random();
+  private Random rand = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
